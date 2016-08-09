a1fb88d45c3b52fc8e613a6fbb77b49bcb03d4e2
Eliezer Abate <code.eliezer@gmail.com>
2016-08-08 19:47:34 -0700

    modified algorithms

--- creative-problems/binary/boilerplate.cc
+++ creative-problems/binary/boilerplate.cc
@@ -2,13 +2,16 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <fstream>
+#include <cassert>
 using namespace std;
 /* 
  * This file generates boiler plate code for 
  * a c++ program, that is this code itself 
  *
  * Usage: ./boilerplate -l c++ [-f filename]
- * -----------------------------------------
+ *          argv[0] | argv[1] | argv[2] | argv[3]
+ * -----------------------------------------------
  *
  * Without the filename options it will generate
  * a file named program.cc
@@ -17,36 +20,75 @@ using namespace std;
 
 enum BoilerType {Default, Custom, Error};
 
-BoilerType getBoilerType(int argc);
-void reportUsageError();
+BoilerType getBoilerType(int argc, char *argv[]);
+void reportUsageError(unsigned int err);
 bool validLanguage(char *lang);
+string toLower(string s);
+
+           
 
 int main(int argc, char *argv[]) {
-  if(argc < 3) {
-    reportUsageError();
-  } else {
-    if()
-  }
+  string boiler_code = string("using namespace std;\n\n") +
+                       "int main(int argc, char *argv[]) {\n" + 
+                       "\treturn 0;\n" +
+                       "}"; /* don't know if valid syntax */ 
 
+  if(argc < 3) {
+    reportUsageError(0);
+  } else {
+    BoilerType bt = getBoilerType(argc,argv);
+    if(bt != Error) {
+      /* we want to place the file in the caller's directory */
+      string s = (bt == Default) ? "boiler.cc" : argv[4];
+      ofstream b_file(s.c_str());
+      if(b_file.is_open()) {
+        b_file << boiler_code;
+      } else {
+        reportUsageError(1);
+      }
+    } else {
+      reportUsageError(0);
+    }
+  }
 }
+
 
 BoilerType getBoilerType(int argc, char *argv[]) {
-  if(argv[1] != "-l") {
-    reportUsageError();
+  assert(argc >= 3);
+  if(strcmp(argv[1], "-l") != 0) {
+    return Error;
+  } 
+  if(!validLanguage(argv[2])) {
+    return Error;
+  } else {
+    if(argc == 5) {
+      if(strcmp(argv[3],"-f") != 0) {
+        return Custom;
+      } 
+    }
+    return Default;
   }
 }
 
-void reportUsageError() {
-  cerr << "Usage: ./boilerplate -l c++ [-f filename]\n";
-  exit(0);
+void reportUsageError(unsigned int err) {
+  assert(err <= 1);
+  switch(err) {
+    case 0:
+      cerr << "Usage: ./boilerplate -l c++ [-f filename]\n";
+      exit(0);
+      break;
+    case 1:
+      cerr << "File Error in of_stream\n";
+      exit(0);
+      break;
+  }
+  
 }
 
 bool validLanguage(char *lang) {
-  string s(lang);
-  string c++ = "c++";
-  if(toLower(s) == "c++") 
+  if(strcmp(string(lang).c_str(),"c++") == 0) 
     return true;
-  return
+  return false;
 
 }
 
